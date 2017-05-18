7b0d0377f6bf472332634993c312bda5358de0ad
wuyuewu515 <946456206@qq.com>
2017-05-18 15:24:42 +0800

    创建application

---
+++ app/src/main/java/com/wyw/zxingdemo/ZXingApplication.java
@@ -0,0 +1,16 @@
+package com.wyw.zxingdemo;
+
+import android.app.Application;
+
+/**
+ * 项目名称：ZXingDemo
+ * 类描述：
+ * 创建人：伍跃武
+ * 创建时间：2017/5/18 15:21
+ */
+public class ZXingApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
